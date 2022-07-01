bebb3f7f8cb03b3dcf6e28422776422c6fced735
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-01 16:30:32 -0300

    <student-test> criando a função teste de student

--- src/functions/student.js
+++ src/functions/student.js
@@ -1,8 +1,18 @@
 // A função createStudent recebe como parâmetro um nome, e retorna um objeto contendo duas chaves:
 
 // 1 - name, contendo o nome passado como parâmetro;
-// 2 - feedback, contendo uma função que retorna a frase 'Eita pessoa boa!' ao ser chamada.
+// 2 - feedback, contendo uma função que retorna a frase 'Bem-vindo NOME!' ao ser chamada.
 
 const createStudent = (name) => {
   if (!name) return undefined;
+  return {
+    name,
+    feedback() {
+      return `Bem-vindo, ${name}!`;
+    },
+  };
 };
+
+console.log(createStudent('Tiago'));
+
+module.exports = createStudent;
