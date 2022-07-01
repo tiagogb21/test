ddecd07c965086a08aad8cc479a92b343342c458
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-01 16:26:12 -0300

    <student> criando a função createStudent

---
+++ src/functions/student.js
@@ -0,0 +1,8 @@
+// A função createStudent recebe como parâmetro um nome, e retorna um objeto contendo duas chaves:
+
+// 1 - name, contendo o nome passado como parâmetro;
+// 2 - feedback, contendo uma função que retorna a frase 'Eita pessoa boa!' ao ser chamada.
+
+const createStudent = (name) => {
+  if (!name) return undefined;
+};
