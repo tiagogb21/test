2d60b56753559f7fea10c2284c6ddda8069859db
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-01 15:48:23 -0300

    <users> implementando a função

---
+++ src/functions/users.js
@@ -0,0 +1,3 @@
+// Função que recebe 2 parâmetros o nome do usuário e a idade dele.
+
+// ex.: Oi, meu nome é Tunico! Tenho 30 anos
--- src/tests/average.test.js
+++ src/tests/average.test.js
@@ -1,15 +1,21 @@
 const average = require('../functions/average');
 
 describe('', () => {
-  test('Verifica se ao receber um array de números retorna a média dos valores', () => {
+  test('1 - Verifica se ao receber um array de números retorna a média dos valores', () => {
     expect(average([1, 2, 3])).toEqual(2);
     expect(average([4, 5, 6])).toEqual(5);
     expect(average([4, 5, 6])).not.toEqual(6);
   });
-  test('Verifica se ao receber um array com valores não numéricos retorna `undefined`', () => {
+  test('2 - Verifica se ao receber um array com valores não numéricos retorna `undefined`', () => {
     expect(average([1, 2, '3'])).toEqual(undefined);
     expect(average(['a', 2, 3])).toEqual(undefined);
     expect(average([' '])).toEqual(undefined);
     expect(average([{}])).toEqual(undefined);
+  });
+  test('3 - Verifica se ao receber um array vazio retorna `undefined', () => {
+    expect(average([])).toEqual(undefined);
+  });
+  test('4 - Verifica se quando não passar parâmetros retorna `undefined', () => {
+    expect(average()).toEqual(undefined);
   });
 });
